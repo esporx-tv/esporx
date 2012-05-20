ac63279ee485dc67944d380ed57f7f8edb14c439
Florent Biville <f.biville@lateral-thoughts.com>
2012-05-20 17:16:07 +0200

    Issue #22 and #24 - make external slot link pop in new tab

--- esporx-webapp/src/main/webapp/static/js/pages/homepage.js
+++ esporx-webapp/src/main/webapp/static/js/pages/homepage.js
@@ -17,7 +17,7 @@ var Homepage = Class.create({
 			$$('#'+highLightsContainerId+' article.'+eventBoxClassName).each(function(element) {
 				element.observe('click', function(event) {
 					var el = Event.element(event);
-					if (el.tagName.toUpperCase() != 'A') { 
+					if (!this.isContainedInALink(el)) { 
 						var link = this.matchingFirstDescendantWithTag(this.matchingFirstParentWithClass(el, eventBoxClassName), 'a');
 						this.redirect(link);
 					}
@@ -26,6 +26,13 @@ var Homepage = Class.create({
 		}
 	},
 
+	/*
+	 * true if element is a link (<a>) or one of his ancestor is a link
+	 */
+	isContainedInALink: function(element) {
+		return element.tagName.toUpperCase() == 'A' || this.matchingFirstParentWithTag(element, 'a') != undefined;
+	},
+	
 	/*
 	 * returns the first provided class-matching parent (provided element included) or undefined if none found
 	 */
@@ -45,6 +52,15 @@ var Homepage = Class.create({
 	/*
 	 * returns the first provided tag name matching parent or undefined if none found
 	 */
+	matchingFirstParentWithTag: function(currentElement, tag) {
+		return currentElement.ancestors().find(function(ancestor) {
+			return ancestor.tagName.toUpperCase() == tag.toUpperCase();
+		});
+	},
+
+	/*
+	 * returns the first provided tag name matching parent or undefined if none found
+	 */
 	matchingFirstDescendantWithTag: function(currentElement, tag) {
 		return currentElement.descendants().find(function(descendant) {
 			return descendant.tagName.toUpperCase() == tag.toUpperCase();
@@ -54,7 +70,13 @@ var Homepage = Class.create({
 	redirect: function(linkElement) {
 		var destination = linkElement.readAttribute('href');
 		if(!destination.blank()) {
-			window.location = destination;
+			var linkDetector = new ExternalLinkDetector();
+			if(linkDetector.isExternalLink(destination)) {
+				window.open(destination);
+			}
+			else {
+				window.location = destination;
+			}
 		}
 	}
 });
