49d0871b2cbffd9eced76b490b13d1e456faae21
Florent Biville <f.biville@lateral-thoughts.com>
2012-06-30 19:10:26 +0200

    BO. Add confirmation before removal

--- esporx-webapp/src/main/webapp/static/js/pages/admin.js
+++ esporx-webapp/src/main/webapp/static/js/pages/admin.js
@@ -1,14 +1,14 @@
-"use strict";
+'use strict';
 var Admin = Class.create({
 	initialize: function() {
 		var controllerName = 'Admin';
 		var adminLogger = new Logger(controllerName);
 		var sanityChecker = new SanityChecker(controllerName);
 		
-		var slideContainerId = "showSlides";
-		var slotContainerId = "showSlots";
-		var channelContainerId = "showChannels";
-		var eventContainerId = "showEvents";
+		var slideContainerId = 'showSlides';
+		var slotContainerId = 'showSlots';
+		var channelContainerId = 'showChannels';
+		var eventContainerId = 'showEvents';
 		
 		var hasErrors = false;
 		hasErrors = sanityChecker.checkIfNotExists('#' + slideContainerId, 'Slide container input')|| hasErrors;
@@ -19,27 +19,35 @@ var Admin = Class.create({
 			adminLogger.error('Script initialization failed due to multiple errors');
 		}
 		else {
-			adminLogger.debug("Initialize admin js");
-			$(slideContainerId).observe("click", function () {
-				this.display($("slidesContainer"));
+			adminLogger.debug('Initialize admin js');
+			$(slideContainerId).observe('click', function () {
+				this.display($('slidesContainer'));
 				this.active($(slideContainerId));
 			}.bind(this));
-			$(slotContainerId).observe("click", function() {
-				this.display($("slotsContainer"));
+			$(slotContainerId).observe('click', function() {
+				this.display($('slotsContainer'));
 				this.active($(slotContainerId));
 			}.bind(this));
-			$(channelContainerId).observe("click", function() {
-				this.display($("channelsContainer"));
+			$(channelContainerId).observe('click', function() {
+				this.display($('channelsContainer'));
 				this.active($(channelContainerId));
 			}.bind(this));
-			$(eventContainerId).observe("click", function() {
-				this.display($("eventsContainer"));
+			$(eventContainerId).observe('click', function() {
+				this.display($('eventsContainer'));
 				this.active($(eventContainerId));
 			}.bind(this));
 			
-			$$(".gondolaRemove").each(function(element) {
-				element.observe("click", function(event) {
-					if(!window.confirm("Do you really want to delete this slide?")) {
+			$$('.gondolaRemove').each(function(element) {
+				element.observe('click', function(event) {
+					if(!window.confirm('Do you really want to delete this slide?')) {
+						Event.stop(event);
+					}
+				});
+			});
+			
+			$$('.send').each(function(element) {
+				element.observe('click', function(event) {
+					if(!window.confirm('Confirm deletion?')) {
 						Event.stop(event);
 					}
 				});
@@ -49,31 +57,29 @@ var Admin = Class.create({
 	},
 
 	display: function(containerToDisplay) {
-		var ArrayContainer = new Array($("slidesContainer"), $("slotsContainer"), $("channelsContainer"), $("eventsContainer"));
-//		var containerToDisplay = this;
-		ArrayContainer.each(function(container) {
+		var arrayContainer = new Array($('slidesContainer'), $('slotsContainer'), $('channelsContainer'), $('eventsContainer'));
+		arrayContainer.each(function(container) {
 			if (container == containerToDisplay) {
-				if (containerToDisplay.hasClassName("displayNone")) containerToDisplay.removeClassName("displayNone");
+				if (containerToDisplay.hasClassName('displayNone')) containerToDisplay.removeClassName('displayNone');
 			} else {
-				if (!container.hasClassName("displayNone")) container.addClassName("displayNone");
+				if (!container.hasClassName('displayNone')) container.addClassName('displayNone');
 			}
 		});
 	},
 	
 	active: function(linkToActive) {
 		console.log('test');
-		var linkContainer = new Array($("showSlides"), $("showSlots"), $("showChannels"), $("showEvents"));
-//		var linkToActive = this;
+		var linkContainer = new Array($('showSlides'), $('showSlots'), $('showChannels'), $('showEvents'));
 		linkContainer.each(function(link) {
 			if (linkToActive == link) {
-				if (!linkToActive.hasClassName("containerSelected"))  {
-					linkToActive.removeClassName("adminName");
-					linkToActive.addClassName("containerSelected");
+				if (!linkToActive.hasClassName('containerSelected'))  {
+					linkToActive.removeClassName('adminName');
+					linkToActive.addClassName('containerSelected');
 				}
 			} else {
-				if (link.hasClassName("containerSelected")) {
-					link.removeClassName("containerSelected");
-					link.addClassName("adminName");
+				if (link.hasClassName('containerSelected')) {
+					link.removeClassName('containerSelected');
+					link.addClassName('adminName');
 				}
 			}
 		});
@@ -81,6 +87,6 @@ var Admin = Class.create({
 });
 
 
-document.observe("dom:loaded",function() {
+document.observe('dom:loaded',function() {
 	new Admin();
 });
