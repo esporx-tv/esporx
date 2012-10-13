4075111cbb96c3eb2b585f20d710712e60756c59
Florent Biville <florent.biville@gmail.com>
2012-10-13 18:31:11 +0200

    Fix the shitty carousel: move the effects part to effects.js!

--- esporx-webapp/src/main/webapp/static/js/ext/carousel.js
+++ esporx-webapp/src/main/webapp/static/js/ext/carousel.js
@@ -294,34 +294,3 @@ Carousel = Class.create(Abstract, {
 		this.controls.invoke('removeClassName', this.options.disabledClassName);
     }
 });
-
-
-Effect.SmoothScroll = Class.create();
-Object.extend(Object.extend(Effect.SmoothScroll.prototype, Effect.Base.prototype), {
-	initialize: function (element) {
-		this.element = $(element);
-		var options = Object.extend({ x: 0, y: 0, mode: 'absolute' } , arguments[1] || {});
-		this.start(options);
-    },
-
-	setup: function () {
-		if (this.options.continuous && !this.element._ext) {
-			this.element.cleanWhitespace();
-			this.element._ext = true;
-			this.element.appendChild(this.element.firstChild);
-        }
-
-		this.originalLeft = this.element.scrollLeft;
-		this.originalTop  = this.element.scrollTop;
-
-		if (this.options.mode == 'absolute') {
-			this.options.x -= this.originalLeft;
-			this.options.y -= this.originalTop;
-        }
-    },
-
-	update: function (position) {
-		this.element.scrollLeft = this.options.x * position + this.originalLeft;
-		this.element.scrollTop  = this.options.y * position + this.originalTop;
-    }
-});
--- esporx-webapp/src/main/webapp/static/js/ext/effects.js
+++ esporx-webapp/src/main/webapp/static/js/ext/effects.js
@@ -1032,6 +1032,36 @@ Effect.Transform = Class.create({
   }
 });
 
+Effect.SmoothScroll = Class.create();
+Object.extend(Object.extend(Effect.SmoothScroll.prototype, Effect.Base.prototype), {
+    initialize: function (element) {
+        this.element = $(element);
+        var options = Object.extend({ x: 0, y: 0, mode: 'absolute' } , arguments[1] || {});
+        this.start(options);
+    },
+
+    setup: function () {
+        if (this.options.continuous && !this.element._ext) {
+            this.element.cleanWhitespace();
+            this.element._ext = true;
+            this.element.appendChild(this.element.firstChild);
+        }
+
+        this.originalLeft = this.element.scrollLeft;
+        this.originalTop  = this.element.scrollTop;
+
+        if (this.options.mode == 'absolute') {
+            this.options.x -= this.originalLeft;
+            this.options.y -= this.originalTop;
+        }
+    },
+
+    update: function (position) {
+        this.element.scrollLeft = this.options.x * position + this.originalLeft;
+        this.element.scrollTop  = this.options.y * position + this.originalTop;
+    }
+});
+
 Element.CSS_PROPERTIES = $w(
   'backgroundColor backgroundPosition borderBottomColor borderBottomStyle ' +
   'borderBottomWidth borderLeftColor borderLeftStyle borderLeftWidth ' +
