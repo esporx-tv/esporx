b1d05de80d15e75eb3d0a8fcff5284c41debd8ba
Florent Biville <florent.biville@gmail.com>
2012-10-14 03:34:36 +0200

    Remove localStorageChecker as LessCSS is not used in the browser anymore

--- esporx-webapp/src/main/webapp/static/js/main.js
+++ esporx-webapp/src/main/webapp/static/js/main.js
@@ -1,5 +1,4 @@
-require(["lib/externalLinkDetector", "lib/localStorageChecker", "ext/prototype"], function(externalLinkDetector, localStorageChecker) {
-    localStorageChecker.check();
+require(["lib/externalLinkDetector", "ext/prototype"], function(externalLinkDetector, localStorageChecker) {
     document.observe('dom:loaded', function() {
         externalLinkDetector.scan();
     });
