bec6fe5209242dcb96cd8a75f91de04bf3e2e798
Florent Biville <f.biville@lateral-thoughts.com>
2012-11-11 13:01:31 +0100

    Try to fix 2 timeline-related tests

---
+++ esporx-webapp/src/main/java/tv/esporx/framework/time/Localizer.java
@@ -0,0 +1,27 @@
+package tv.esporx.framework.time;
+
+import org.joda.time.DateTimeZone;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import java.util.Locale;
+import java.util.TimeZone;
+
+import static java.util.Locale.ENGLISH;
+import static org.springframework.core.Ordered.HIGHEST_PRECEDENCE;
+
+@Component
+@Lazy(false)
+@Order(HIGHEST_PRECEDENCE)
+public class Localizer {
+
+	public Localizer() {
+        DateTimeZone zone = DateTimeZone.forID("Europe/Paris");
+        TimeZone.setDefault(zone.toTimeZone());
+        DateTimeZone.setDefault(zone);
+        Locale.setDefault(ENGLISH);
+	}
+
+}
