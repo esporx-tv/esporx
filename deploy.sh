cdad74ac445540c585442141bd8fd58e17575b25
Florent Biville <f.biville@lateral-thoughts.com>
2013-02-12 19:33:40 +0100

    Edit script + add good Tomcat Maven plugin

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,24 @@
 export JAVA_OPTS="-server -Xmx512m -XX:MaxPermSize=512M -XX:+UseConcMarkSweepGC -XX:+CMSClassUnloadingEnabled"
 : ${TOMCAT_HOME?"You should define TOMCAT_HOME as your Tomcat base directory"}
 
-if [ -f target/esporx_tv.war ]; then
-	$TOMCAT_HOME/bin/shutdown.sh
-	cp target/esporx_tv.war $TOMCAT_HOME/webapps/ROOT.war
-	$TOMCAT_HOME/bin/catalina.sh start
-else
+if [ ! -d $TOMCAT_HOME ]; then
+	echo "The TOMCAT_HOME environment variable does not point to a valid directory"
+	exit -1
+fi 
+
+if [ ! -f target/esporx_tv.war ]; then
 	echo "WAR not found: you should first package the app!"
+	exit -1
 fi
+
+echo ""
+echo "Halting Tomcat"	
+echo ""
+$TOMCAT_HOME/bin/shutdown.sh
+
+cp target/esporx_tv.war $TOMCAT_HOME/webapps/ROOT.war
+
+echo ""
+echo "Starting Tomcat again"
+echo ""
+$TOMCAT_HOME/bin/catalina.sh start
